45b3388bd5ddab16953ce4a826b73cf9e2655c76
PerpetuumG <perpetuumg@gmail.com>
2025-06-12 21:37:10 +0300

    create project

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Calendra',
+  description:
+    'Calendra — это простое и эффективное приложение-календарь, которое поможет вам с легкостью управлять своими событиями, встречами и расписанием. Будьте организованны и больше никогда не пропустите важную дату!',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
