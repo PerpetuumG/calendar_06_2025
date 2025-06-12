afe7b42dd959f9d82e749567c5db34c54d430249
PerpetuumG <perpetuumg@gmail.com>
2025-06-12 22:03:27 +0300

    login

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // Пропустить внутренние компоненты Next.js и все статические файлы, если они не найдены в параметрах поиска.
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
