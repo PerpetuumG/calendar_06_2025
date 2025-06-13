9abad1230b97975b0ca22370c3dbc45d27c24f99
PerpetuumG <perpetuumg@gmail.com>
2025-06-13 11:23:27 +0300

    publicNavBar

---
+++ app/(main)/(public)/book/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>Book page</h1>;
+};
+
+export default Page;
---
+++ components/PrivateNavBar.tsx
@@ -0,0 +1,5 @@
+const PrivateNavBar = () => {
+  return <div></div>;
+};
+
+export default PrivateNavBar;
