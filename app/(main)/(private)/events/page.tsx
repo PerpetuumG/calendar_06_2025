d39c349ef0bdc8f75664525ec2e913cbc4fd2723
PerpetuumG <perpetuumg@gmail.com>
2025-06-13 12:56:38 +0300

    events page

--- app/(main)/(private)/events/page.tsx
+++ app/(main)/(private)/events/page.tsx
@@ -1,5 +1,31 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { CalendarPlus } from 'lucide-react';
+
 const Page = () => {
-  return <h1>Events</h1>;
+  return (
+    <section className={'flex flex-col items-center gap-16 animate-fade-in'}>
+      <div className={'flex gap-4 items-baseline'}>
+        <h1 className={'text-4xl xl:text-5xl font-black mb-6'}>События</h1>
+        {/*
+        Без asChild кнопка будет отображаться следующим образом:
+        <button><a href="/dashboard">Go to Dashboard</a></button> <!-- Invalid HTML -->
+        С asChild он отображается следующим образом:
+        <a href="/dashboard" class="...button styles...">Go to Dashboard</a> <!-- Valid HTML -->
+        Это полезно, когда вы хотите, чтобы другой элемент (например, <Link>) выглядел и вел себя как кнопка, не нарушая семантику HTML.*/}
+        <Button
+          className={
+            'bg-blue-500 hover:bg-blue-400 text-white py-6 hover:scale-110 duration-500 border-b-4 border-blue-700 hover:border-blue-500 rounded-2xl shadow-accent-foreground text-2xl font-black'
+          }
+          asChild
+        >
+          <Link href={'/events/new'}>
+            <CalendarPlus className={'mr-4 size-7'} /> Создать событие
+          </Link>
+        </Button>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
