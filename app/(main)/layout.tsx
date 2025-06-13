d58341e3b016e8b2a04daec616be555485ff9605
PerpetuumG <perpetuumg@gmail.com>
2025-06-13 12:41:07 +0300

    Navigation Bars

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { currentUser } from '@clerk/nextjs/server';
 import PublicNavBar from '@/components/PublicNavBar';
+import PrivateNavBar from '@/components/PrivateNavBar';
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   const user = await currentUser();
 
   return (
     <main className={'relative'}>
-      <PublicNavBar />
       {/* Отобразить PrivateNavBar, если пользователь существует, в противном случае PublicNavBar */}
-      {/*{user ? <PrivateNavBar /> : <PublicNavBar />}*/}
+      {user ? <PrivateNavBar /> : <PublicNavBar />}
 
       <section className={'pt-36'}>{children}</section>
     </main>
--- constants/index.ts
+++ constants/index.ts
@@ -7,3 +7,21 @@ export const DAYS_OF_WEEK_IN_ORDER = [
   'saturday',
   'sunday',
 ] as const;
+
+export const PrivateNavLinks = [
+  {
+    imgURL: '/assets/events.svg',
+    route: '/events',
+    label: 'Мои события',
+  },
+  {
+    imgURL: '/assets/schedule.svg',
+    route: '/schedule',
+    label: 'Мое расписание',
+  },
+  {
+    imgURL: '/assets/public.svg',
+    route: '/book',
+    label: 'Публичный профиль',
+  },
+] as const;
